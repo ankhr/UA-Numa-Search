13564a399f9bc35c6552f0c23337146de94c0bb2
ankhr <alton.atlas@gmail.com>
2018-05-02 15:49:52 -0500

    Added HashMap and while for moving forward

--- src/pageRank/PageNameParser.java
+++ src/pageRank/PageNameParser.java
@@ -11,12 +11,14 @@ import java.nio.charset.Charset;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 
 public class PageNameParser {
     
     public static void main (String[] args) {
         
+        HashMap<Integer, String> map = new HashMap<>();
         try {
             JSch jsch = new JSch();
             Session session = jsch.getSession("uaserver", "74.117.171.100", 22);
@@ -37,24 +39,31 @@ public class PageNameParser {
                 
                 String line;
                 int count = 0;
+                String[] pageInfo = new String[2];
                 while ((line = br.readLine()) != null) {
 //                    bw.write(line+"\n");
                     String title[] = StringUtils.substringsBetween(line, "<title>", "</title>");
                     String id[] = StringUtils.substringsBetween(line, "<id>", "</id>");
+                    
                     if (title != null) {
                         for (int i=0; i<title.length; i++) {
-                            System.out.print(title[i]+","+"\n");
+                            System.out.print(title[i]+"\n");
+                            pageInfo[1] = title[i];
 //                            pages.put(links[i], new PageNode(links[i]));
                         }
                     }
                     if (id != null) {
                         for (int i=0; i<id.length; i++) {
-                            System.out.print(id[i]+","+"\n");
+                            System.out.print(id[i]+", ");
+                            pageInfo[0] = id[i];
                             //pages.put(links[i], new PageNode(links[i]));
-                            for (int j=0; j<8; j++) {
+                            while (!line.trim().equalsIgnoreCase("<page>")) {
                                 line = br.readLine();
                             }
                         }
+                    }
+                    if (pageInfo[0] != null && pageInfo[1] != null) {
+                        map.put(Integer.parseInt(pageInfo[0]), pageInfo[1]);
                     }
                     count++;
                 }
@@ -62,6 +71,7 @@ public class PageNameParser {
 //                bw.close();
             }catch (Exception e) {
                 System.out.println("Exception occurred during reading file from SFTP server due to " + e.getMessage());
+                e.printStackTrace();
             }
             
             
@@ -72,5 +82,15 @@ public class PageNameParser {
         } catch (SftpException e) {
             e.printStackTrace();
         }
+        
+        System.out.println();
+        System.out.println("1189, " + map.get(12));
+    
+        System.out.println("25, " + map.get(25));
+    
+        System.out.println("36, " + map.get(36));
+        
+        
+        
     }
 }
